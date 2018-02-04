693325ad6c76932518f15fa1cd99ec50cf75790a
wangpx123 <499700474@qq.com>
2018-02-05 00:42:44 +0800

    del

--- src/test/java/com/crm/CrmApplicationTests.java
+++ src/test/java/com/crm/CrmApplicationTests.java
@@ -1,5 +1,9 @@
 package com.crm;
 
+import com.crm.controller.UserController;
+import com.crm.service.UserService;
+import com.crm.service.impl.UserServiceImpl;
+import com.crm.vo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +13,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CrmApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
+    @Test
+    public void contextLoads() {
+        User user = new User();
+        user.setUpassword("123");
+        user.setUsername("wpx");
+        user.setUseraccont("wpx");
+//		UserController userController = new UserController();
+        UserService us = new UserServiceImpl();
+        us.addUser(user);
+    }
 
 }
